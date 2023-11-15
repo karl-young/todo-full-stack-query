397a993eb7634bae5bfcd1b073933039ed04aa44
Karl Young <Karlyoung112@gmail.com>
2023-11-15 22:07:57 +1300

    Added code for form and app but is broken by Link?

--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -1,7 +1,41 @@
 // eslint-disable-next-line no-unused-vars
-function AddTodo() {
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { Task, TaskData } from '../../models/taskModels'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addTask } from '../apis/task'
 
-  
+const initialFormData = {
+  task: '',
+  completed: false,
+}
+
+function AddTodo() {
+  const [form, setForm] = useState<TaskData>(initialFormData)
+  const queryClient = useQueryClient()
+
+  const taskMutation = useMutation({
+    mutationFn: addTask,
+    onSuccess: (newTask) => {
+      queryClient.invalidateQueries({ queryKey: ['task'] })
+    },
+  })
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target
+    const newForm = { ...form, [name]: value }
+    setForm(newForm)
+  }
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    taskMutation.mutate(form)
+    setForm(initialFormData)
+  }
+
+  if (taskMutation.isLoading) {
+    return <p>Adding Task</p>
+  }
+
   return (
     <>
       <input className="new-todo" placeholder="What needs to be done?" />
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,40 @@
 import AddTodo from './AddTodo.tsx'
+import { useQuery } from '@tanstack/react-query'
+import { getTasks } from '../apis/task.ts'
+import { Link } from 'react-router-dom'
 
 function App() {
-  
+  const {
+    data: tasks,
+    isLoading,
+    error,
+  } = useQuery({ queryKey: ['tasks'], queryFn: getTasks })
+
+  if (error) {
+    return <p>An error has occurred</p>
+  }
+
+  if (isLoading || !tasks) {
+    return <p>Loading...</p>
+  }
+
   return (
     <>
       <header className="header">
         <h1>Task List</h1>
         <AddTodo />
       </header>
-      <section className="main"></section>
+      <section className="main">
+        <ul className="todo-list">
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <p>
+                <Link to={`/tasks/${task.id}`}> {task.task} {task.completed}</Link>
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
       <footer className="footer"></footer>
     </>
   )
